c0eedb84eabe261b5a7d6aab799ad628e651fcf8
pinky <pinkipandarojo@gmail.com>
2025-08-23 07:57:04 -0400

    realistic

--- levels.py
+++ levels.py
@@ -25,34 +25,5 @@ levels = [
     LevelData(
         columns=[*add_columns_around_pig(920, 100), *add_columns_around_pig(1200, 100)],
         pigs=[(920, 100), (1200, 100)],
-    ),
-    LevelData(
-        columns=[*add_columns_around_pig(800, 100), *add_columns_around_pig(1200, 100)],
-        pigs=[(800, 100), (1200, 100)],
-    ),
-    LevelData(
-        columns=[
-            *add_columns_around_pig(700, 100),
-            *add_columns_around_pig(1000, 100),
-            *add_columns_around_pig(1300, 100),
-            (850, 50),
-            (850, COLUMN_HEIGHT+50),
-            (850, COLUMN_HEIGHT*2+50),
-            (850, COLUMN_HEIGHT*3+50),
-            (850, COLUMN_HEIGHT*4+50),
-            (850, COLUMN_HEIGHT*5+50),
-        ],
-        pigs=[(700, 100), (1000, 100), (1300, 100)],
-    ),
-    LevelData(
-        columns=[
-            *add_columns_around_pig(600, 100),
-            *add_columns_around_pig(950, 100),
-            *add_columns_around_pig(1300, 100),
-            (775, 50),
-            (1125, 50),
-            (1475, 50), 
-        ],
-        pigs=[(600, 100), (950, 100), (1300, 100)],
-    ),
+    )
 ]
